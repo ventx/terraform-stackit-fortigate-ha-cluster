6fa280239ce0e7ffa6c10679a1997948eb6a1f51
Florian Beier <florian@ventx.de>
2025-10-29 01:03:19 +0100

    Add HA setup

--- main.tf
+++ main.tf
@@ -4,13 +4,129 @@ resource "stackit_resourcemanager_project" "fortigate" {
   owner_email         = var.owner_email
 }
 
-resource "stackit_network" "fortigate" {
+resource "stackit_network" "private01" {
   project_id       = stackit_resourcemanager_project.fortigate.project_id
-  name             = "nw-fortigate-${var.environment}"
+  name             = "nw-private01-${var.environment}"
   ipv4_nameservers = ["9.9.9.9"]
-  ipv4_prefix      = "10.0.0.0/24"
+  ipv4_prefix      = "172.31.0.0/24"
   routed           = true
 }
+
+resource "stackit_network" "r" {
+  project_id       = stackit_resourcemanager_project.fortigate.project_id
+  name             = "nw-network-r-${var.environment}"
+  ipv4_nameservers = ["9.9.9.9"]
+  ipv4_prefix      = "172.32.0.0/24"
+  routed           = false
+}
+
+resource "stackit_network" "l" {
+  project_id       = stackit_resourcemanager_project.fortigate.project_id
+  name             = "nw-network-l-${var.environment}"
+  ipv4_nameservers = ["9.9.9.9"]
+  ipv4_prefix      = "172.33.0.0/24"
+  routed           = false
+}
+
+resource "stackit_network" "ha_sync" {
+  project_id       = stackit_resourcemanager_project.fortigate.project_id
+  name             = "nw-ha-sync-${var.environment}"
+  ipv4_nameservers = ["9.9.9.9"]
+  ipv4_prefix      = "169.254.0.0/24"
+  routed           = false
+}
+
+resource "stackit_security_group" "web" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  name       = "sg-web-${var.environment}"
+  stateful   = true
+}
+
+resource "stackit_security_group_rule" "ssh" {
+  project_id        = stackit_resourcemanager_project.fortigate.project_id
+  security_group_id = stackit_security_group.web.security_group_id
+  direction         = "ingress"
+  description       = "Allow SSH"
+  protocol = {
+    name = "tcp"
+  }
+  port_range = {
+    max = 22
+    min = 22
+  }
+}
+
+resource "stackit_image" "alpine" {
+  project_id      = stackit_resourcemanager_project.fortigate.project_id
+  name            = "img-alpine-${var.environment}"
+  disk_format     = "qcow2"
+  local_file_path = "alpine.qcow2"
+  min_disk_size   = 1
+  min_ram         = 128
+}
+
+resource "stackit_key_pair" "main" {
+  name       = "key-main-${var.environment}"
+  public_key = chomp(file(var.public_key_path))
+}
+
+# Alpine VMs
+
+resource "stackit_network_interface" "alpine_r" {
+  project_id         = stackit_resourcemanager_project.fortigate.project_id
+  network_id         = stackit_network.r.network_id
+  security_group_ids = [stackit_security_group.web.security_group_id]
+  ipv4               = "172.32.0.5"
+}
+
+resource "stackit_public_ip" "alpine_r" {
+  project_id           = stackit_resourcemanager_project.fortigate.project_id
+  network_interface_id = stackit_network_interface.alpine_r.network_interface_id
+}
+
+resource "stackit_server" "alpine_r" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  boot_volume = {
+    size                  = 1
+    source_type           = "image"
+    source_id             = stackit_image.alpine.image_id
+    performance_class     = "storage_premium_perf0"
+    delete_on_termination = true
+  }
+  name               = "vm-alpine-r-${var.environment}"
+  machine_type       = "c2i.1"
+  keypair_name       = stackit_key_pair.main.name
+  network_interfaces = [stackit_network_interface.alpine_r.network_interface_id]
+}
+
+resource "stackit_network_interface" "alpine_l" {
+  project_id         = stackit_resourcemanager_project.fortigate.project_id
+  network_id         = stackit_network.l.network_id
+  security_group_ids = [stackit_security_group.web.security_group_id]
+  ipv4               = "172.33.0.5"
+}
+
+resource "stackit_public_ip" "alpine_l" {
+  project_id           = stackit_resourcemanager_project.fortigate.project_id
+  network_interface_id = stackit_network_interface.alpine_l.network_interface_id
+}
+
+resource "stackit_server" "alpine_l" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  boot_volume = {
+    size                  = 1
+    source_type           = "image"
+    source_id             = stackit_image.alpine.image_id
+    performance_class     = "storage_premium_perf0"
+    delete_on_termination = true
+  }
+  name               = "vm-alpine-l-${var.environment}"
+  machine_type       = "c2i.1"
+  keypair_name       = stackit_key_pair.main.name
+  network_interfaces = [stackit_network_interface.alpine_l.network_interface_id]
+}
+
+# FortiGate VMs
 
 resource "stackit_image" "fortios" {
   project_id      = stackit_resourcemanager_project.fortigate.project_id
@@ -21,57 +137,36 @@ resource "stackit_image" "fortios" {
   min_ram         = 2048
 }
 
-resource "stackit_security_group" "fortigate_public" {
+resource "stackit_network_interface" "fortigate1_private01" {
   project_id = stackit_resourcemanager_project.fortigate.project_id
-  name       = "sg-fortigate-public-${var.environment}"
-  stateful   = true
+  network_id = stackit_network.private01.network_id
+  security   = false
 }
 
-resource "stackit_security_group" "fortigate_internal" {
+resource "stackit_network_interface" "fortigate1_network_r" {
   project_id = stackit_resourcemanager_project.fortigate.project_id
-  name       = "sg-fortigate-internal-${var.environment}"
-  stateful   = true
+  network_id = stackit_network.r.network_id
+  security   = false
 }
 
-resource "stackit_network_interface" "fortigate_public" {
-  project_id         = stackit_resourcemanager_project.fortigate.project_id
-  network_id         = stackit_network.fortigate.network_id
-  security_group_ids = [stackit_security_group.fortigate_public.security_group_id]
+resource "stackit_network_interface" "fortigate1_network_l" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  network_id = stackit_network.l.network_id
+  security   = false
 }
 
-resource "stackit_network_interface" "fortigate_internal" {
-  count = 2
-
-  project_id         = stackit_resourcemanager_project.fortigate.project_id
-  network_id         = stackit_network.fortigate.network_id
-  security_group_ids = [stackit_security_group.fortigate_internal.security_group_id]
+resource "stackit_network_interface" "fortigate1_ha_sync" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  network_id = stackit_network.ha_sync.network_id
+  security   = false
 }
 
-resource "stackit_public_ip" "fortigate" {
+resource "stackit_public_ip" "fortigate1" {
   project_id           = stackit_resourcemanager_project.fortigate.project_id
-  network_interface_id = stackit_network_interface.fortigate_public.network_interface_id
+  network_interface_id = stackit_network_interface.fortigate1_private01.network_interface_id
 }
 
-resource "stackit_key_pair" "fortigate" {
-  name       = "key-fortigate-${var.environment}"
-  public_key = chomp(file(var.public_key_path))
-}
-
-resource "stackit_volume" "fortios_logs" {
-  project_id        = stackit_resourcemanager_project.fortigate.project_id
-  name              = "vol-fortios-logs-${var.environment}"
-  availability_zone = stackit_server.fortigate.availability_zone
-  size              = 32
-  performance_class = "storage_premium_perf0"
-}
-
-resource "stackit_server_volume_attach" "fortios_logs" {
-  project_id = stackit_resourcemanager_project.fortigate.project_id
-  server_id  = stackit_server.fortigate.server_id
-  volume_id  = stackit_volume.fortios_logs.volume_id
-}
-
-resource "stackit_server" "fortigate" {
+resource "stackit_server" "fortigate1" {
   project_id = stackit_resourcemanager_project.fortigate.project_id
   boot_volume = {
     size                  = 2
@@ -81,11 +176,197 @@ resource "stackit_server" "fortigate" {
     delete_on_termination = true
   }
   availability_zone = "eu01-1"
-  name              = "ser-fortigate-${var.environment}"
-  machine_type      = "c1a.1d"
-  keypair_name      = stackit_key_pair.fortigate.name
-  network_interfaces = concat(
-    [stackit_network_interface.fortigate_public.network_interface_id],
-    stackit_network_interface.fortigate_internal[*].network_interface_id
-  )
+  name              = "ser-fortigate1-${var.environment}"
+  machine_type      = "c2i.1"
+  keypair_name      = stackit_key_pair.main.name
+  network_interfaces = [
+    stackit_network_interface.fortigate1_private01.network_interface_id,
+    stackit_network_interface.fortigate1_network_r.network_interface_id,
+    stackit_network_interface.fortigate1_network_l.network_interface_id,
+    stackit_network_interface.fortigate1_ha_sync.network_interface_id,
+  ]
+  user_data = <<-EOT
+  config sys global
+    set hostname fortigate1
+  end
+  config system interface
+    edit port1
+      set mode static
+      set ip 172.31.0.3 255.255.255.0
+      set allowaccess http https ssh ping
+    next
+    edit port2
+      set mode static
+      set ip 172.32.0.3 255.255.255.0
+      set defaultgw disable
+      set allowaccess http https ssh ping
+    next
+    edit port3
+      set mode static
+      set ip 172.33.0.3 255.255.255.0
+      set defaultgw disable
+      set allowaccess http https ssh ping
+    next
+    edit port4
+      set mtu-override enable
+      set mtu 1400
+    next
+  end
+  config router static
+    edit 1
+      set gateway 172.31.0.1
+      set device "port1"
+  end
+  config system dns
+    set primary 9.9.9.9
+  end
+  config system ha
+    set group-name "group-01"
+    set mode a-p
+    set password "qfNTDkjKHkZre5VSWKJi"
+    set hbdev "port4" 50
+    set override disable
+    set monitor "port2"
+  end
+  config firewall policy
+    edit 1
+      set name "Allow port2 to port3"
+      set dstintf "port2"
+      set srcintf "port3"
+      set srcaddr "all"
+      set dstaddr "all"
+      set action accept
+      set schedule "always"
+      set service "ALL"
+      set nat enable
+    next
+    edit 2
+      set name "Allow port3 to port2"
+      set dstintf "port3"
+      set srcintf "port2"
+      set srcaddr "all"
+      set dstaddr "all"
+      set action accept
+      set schedule "always"
+      set service "ALL"
+      set nat enable
+  end
+  EOT
+}
+
+resource "stackit_network_interface" "fortigate2_private01" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  network_id = stackit_network.private01.network_id
+  security   = false
+}
+
+resource "stackit_network_interface" "fortigate2_network_r" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  network_id = stackit_network.r.network_id
+  security   = false
+}
+
+resource "stackit_network_interface" "fortigate2_network_l" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  network_id = stackit_network.l.network_id
+  security   = false
+}
+
+resource "stackit_network_interface" "fortigate2_ha_sync" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  network_id = stackit_network.ha_sync.network_id
+  security   = false
+}
+
+resource "stackit_public_ip" "fortigate2" {
+  project_id           = stackit_resourcemanager_project.fortigate.project_id
+  network_interface_id = stackit_network_interface.fortigate2_private01.network_interface_id
+}
+
+resource "stackit_server" "fortigate2" {
+  project_id = stackit_resourcemanager_project.fortigate.project_id
+  boot_volume = {
+    size                  = 2
+    source_type           = "image"
+    source_id             = stackit_image.fortios.image_id
+    performance_class     = "storage_premium_perf0"
+    delete_on_termination = true
+  }
+  availability_zone = "eu01-2"
+  name              = "ser-fortigate2-${var.environment}"
+  machine_type      = "c2i.1"
+  keypair_name      = stackit_key_pair.main.name
+  network_interfaces = [
+    stackit_network_interface.fortigate2_private01.network_interface_id,
+    stackit_network_interface.fortigate2_network_r.network_interface_id,
+    stackit_network_interface.fortigate2_network_l.network_interface_id,
+    stackit_network_interface.fortigate2_ha_sync.network_interface_id,
+  ]
+  user_data = <<-EOT
+  config sys global
+    set hostname fortigate2
+  end
+  config system interface
+    edit port1
+      set mode static
+      set ip 172.31.0.4 255.255.255.0
+      set allowaccess http https ssh ping
+    next
+    edit port2
+      set mode static
+      set ip 172.32.0.4 255.255.255.0
+      set defaultgw disable
+      set allowaccess http https ssh ping
+    next
+    edit port3
+      set mode static
+      set ip 172.33.0.4 255.255.255.0
+      set defaultgw disable
+      set allowaccess http https ssh ping
+    next
+    edit port4
+      set mtu-override enable
+      set mtu 1400
+    next
+  end
+  config router static
+    edit 1
+      set gateway 172.31.0.1
+      set device "port1"
+  end
+  config system dns
+    set primary 9.9.9.9
+  end
+  config system ha
+    set group-name "group-01"
+    set mode a-p
+    set password "qfNTDkjKHkZre5VSWKJi"
+    set hbdev "port4" 50
+    set override disable
+    set monitor "port2"
+  end
+  config firewall policy
+    edit 1
+      set name "Allow port2 to port3"
+      set dstintf "port2"
+      set srcintf "port3"
+      set srcaddr "all"
+      set dstaddr "all"
+      set action accept
+      set schedule "always"
+      set service "ALL"
+      set nat enable
+    next
+    edit 2
+      set name "Allow port3 to port2"
+      set dstintf "port3"
+      set srcintf "port2"
+      set srcaddr "all"
+      set dstaddr "all"
+      set action accept
+      set schedule "always"
+      set service "ALL"
+      set nat enable
+  end
+  EOT
 }
